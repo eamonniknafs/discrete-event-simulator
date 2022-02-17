0c8463ed767a07973b370af9840ba60d625d4597
Eamon Niknafs <en@bu.edu>
2022-02-17 02:24:35 -0500

    Added details for Timeline class, updated State

--- State.java
+++ State.java
@@ -1,10 +1,17 @@
 import java.util.PriorityQueue;
 
 public class State {
+    PriorityQueue<Request> timeline;
+    int tot_qu_len;
+    Double tot_rq_time;
+    Double busy_time;
+    int tot_rq_num;
+
     void initiateState() {
-        PriorityQueue<Request> timeline = new PriorityQueue<Request>();
-        int tot_qu_len = 0;
-        Double tot_rq_time = 0.0;
-        Double busy_time = 0.0;
+        timeline = new PriorityQueue<Request>();
+        tot_qu_len = 0;
+        tot_rq_time = 0.0;
+        busy_time = 0.0;
+        tot_rq_num = 0;
     }
 }
--- Timeline.java
+++ Timeline.java
@@ -1,4 +1,11 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Timeline {
-    
-    
+    Queue<Event> timeline;
+    void initiateTimeline(){
+        timeline = new LinkedList<Event>();
+        timeline.add(new Event("BIRTH"));
+        timeline.add(new Event("MONITOR"));
+    }
 }
